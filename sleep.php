910ba6b3d4eeb31b0baceec7e5eef338a84bb5de
Hilmi Esen <hilmiesen@gmail.com>
2026-08-28 23:23:35 +0300

    Refactor sleep.php for millisecond sleep and response time
    Updated sleep functionality to use usleep for millisecond precision and added response time calculation.

--- sleep.php
+++ sleep.php
@@ -1,6 +1,17 @@
 <?php
-if(isset($_GET["s"]) &&  is_numeric($_GET["s"])) $sn = $_GET["s"];
-else $sn = 5;	
-sleep($sn);
-echo "sn: $sn . Merhaba Dunya";
+$start = microtime(true);
+if (isset($_GET["ms"]) && is_numeric($_GET["ms"])) {
+    $ms = $_GET["ms"];
+} else {
+    $ms = 2000;
+}
+usleep($ms * 1000);
+$response_time = (microtime(true) - $start) * 1000;
+
+echo "ms: $ms . Merhaba Dunya<br><br>";
+echo "Kullanımı: Gelen isteği default 2000 ms bekletir ve sonra ekrana Merhaba Dünya basar.<br>";
+echo "Süreyi ms parametresi ekleyerek kontrol edebilirsiniz.<br>";
+echo "Örneğin 500ms için: /sleep.php?ms=500 gibi.<br><br><br>";
+echo "Response Time: " . round($response_time, 2) . " ms";
+
 ?>
